27b95174b014a7fa0c7c261eed79891e84148620
Victor Calderon <2266703+vcalderon2009@users.noreply.github.com>
2023-03-07 02:08:34 -0600

    Initial refactoring (#1)
    * Adding file for pre-commit
    
    * Adding initial file for 'direnv'
    
    * Upating set of package requirements
    
    * Adding files for code-linting and code-checking
    
    * Updating '.gitignore' to include environment-specific files
    
    * Adding 'Makefile' with useful functions to interact with the project
    
    * Updated 'README' with descripttion and set of functions
    
    * Adding workflow for code-linting and code-checking
    
    * Updated 'Makefile' with function to create new coding problem
    
    * Initial add of the 'assets' directory
    
    * Reduced the number of python packages
    
    * Added sample script for coding problems
    
    * Added script that makes a copy of the sample script into the corresponding directory within 'src'
    
    * Updated README file with instructions on how to add new scripts
    
    * Adding '.gitkeep' to the 'src' directory to make it part of the repo
    
    * Updated docstring for the sample script
    
    * Updated function for removing pre-commit
    
    * Renamed title of the 'README' file
    
    * Added version number of 'numpy' to package requirements

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# job_coding_exercises
-Repository for practicing coding exercises
+# Coding Exercises
+
+This repository is designed for practicing coding problems during
+coding interviews.
+
+- [Instructions for local development](#instructions-for-local-development)
+
+## Instructions for local development
+
+The repository comes with a `Makefile` that contains a set of functions
+used for coding practices.
+
+The `Makefile` comes with the following set of functions:
+
+```bash
+(base) ➜  job_coding_exercises git:(main ✗ make help
+Available rules:
+
+clean                     Removes artifacts from the build stage, and other common Python artifacts.
+clean-build               Remove build artifacts
+clean-pyc                 Removes Python file artifacts
+clean-test                Remove test and coverage artifacts
+create-environment        Creates the Python environment
+create-envrc              Set up the envrc file for the project.
+delete-environment        Deletes the Python environment
+delete-envrc              Delete the local envrc file of the project
+destroy                   Remove ALL of the artifacts + Python environments
+init                      Initialize the repository for code development
+lint                      Run the 'pre-commit' linting step manually
+new-problem               Function to create a file for a new coding problem
+pip-upgrade               Upgrade the version of the 'pip' package
+pre-commit-install        Installing the pre-commit Git hook
+pre-commit-uninstall      Uninstall the pre-commit Git hook
+requirements              Install Python dependencies into the Python environment
+show-params               Show the set of input parameters
+sort-requirements         Sort the project packages requirements file
+```
+
+Each of these functions can be accessed through the `make` command at the
+base directory level.
+
+### Create new solution
+
+In order to create a file for the solution of a speciified coding exercise,
+one can run the following command:
+
+```bash
+make new-problem COMPANY_NAME="company" PROBLE_TYPE="type-of-problem" YEAR="2023" TITLE="name-of-problem"
+```
+, where:
+
+- `COMPANY_NAME` corresponds to the name of the company, to which one is applying;
+- `PROBLE_TYPE` corresponds to the type of coding problem ;
+- `YEAR` corresponds to the year of submission / application;
+- `TITLE` corresponds to the name of the script.
+
+This command will copy the sample script into the corresponding folder and
+have it named accordingly.
---
+++ assets/create_challenge_file.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# MIT License
+#
+# Copyright (c) 2023 Victor Calderon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Created       : 2023-03-07
+# Last Modified : 2023-03-07
+
+import argparse
+import logging
+import shutil
+from argparse import ArgumentParser, HelpFormatter
+from datetime import datetime
+from operator import attrgetter
+from pathlib import Path
+from typing import Dict, Optional
+
+import numpy as np
+
+__author__ = ["Victor Calderon"]
+__copyright__ = ["Copyright 2023"]
+__maintainer__ = ["Victor Calderon"]
+__all__ = []
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+)
+logger.setLevel(logging.INFO)
+
+# ----------------------------- INPUT PARAMETERS ------------------------------
+
+
+def _str2bool(v):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+class SortingHelpFormatter(HelpFormatter):
+    def add_arguments(self, actions):
+        """
+        Modifier for `argparse` help parameters, that sorts them alphabetically
+        """
+        actions = sorted(actions, key=attrgetter("option_strings"))
+        super(SortingHelpFormatter, self).add_arguments(actions)
+
+
+def get_parser():
+    """
+    Function to get the input parameters to the script.
+    """
+    description = ""
+    parser = ArgumentParser(
+        description=description,
+        formatter_class=SortingHelpFormatter,
+    )
+    # Name of the company
+    parser.add_argument(
+        "--company",
+        dest="company_name",
+        default="general",
+        type=str,
+        help="""
+        Name of the company, to which one is applying
+        [Default: '%(default)s']
+        """,
+    )
+    # Problem type
+    parser.add_argument(
+        "--problem-type",
+        dest="problem_type",
+        default="general",
+        type=str,
+        help="""
+        Type of coding problem.
+        [Default: '%(default)s']
+        """,
+    )
+    # Year of submission
+    parser.add_argument(
+        "--year",
+        dest="submission_year",
+        default=datetime.now().strftime("%Y"),
+        type=str,
+        help="""
+        Year of submission
+        [Default: '%(default)s']
+        """,
+    )
+    # Title of the coding problem
+    parser.add_argument(
+        "--title",
+        dest="title",
+        default="",
+        type=str,
+        help="""
+        Title to use for the coding problem.
+        [Default: '%(default)s']
+        """,
+    )
+
+    return parser.parse_args()
+
+
+# ---------------------------- GENERAL FUNCTIONS ------------------------------
+
+
+def _show_params(params_dict: Dict):
+    """
+    Function to show the defined of the class.
+    """
+    msg = "-" * 50 + "\n"
+    msg += "\t---- INPUT PARAMETERS ----" + "\n"
+    msg += "" + "\n"
+    # Keys to omit
+    columns_to_omit = []
+    # Sorting keys of dictionary
+    keys_sorted = np.sort(list(params_dict.keys()))
+    for key_ii in keys_sorted:
+        if key_ii not in columns_to_omit:
+            msg += f"\t>>> {key_ii} : {params_dict[key_ii]}\n"
+    #
+    msg += "\n" + "-" * 50 + "\n"
+    logger.info(msg)
+
+    return
+
+
+class CreateCopyFile(object):
+    """
+    Class object for making a copy of the ``sample_problems.py`` script.
+    """
+
+    def __init__(self, **kwargs) -> None:
+        # Initializing parameters
+        self.__dict__.update(**kwargs)
+
+    def _show_params(self):
+        """
+        Function to show the defined of the class.
+        """
+        msg = "-" * 50 + "\n"
+        msg += "\t---- INPUT PARAMETERS ----" + "\n"
+        msg += "" + "\n"
+        # Keys to omit
+        columns_to_omit = []
+        # Sorting keys of dictionary
+        keys_sorted = np.sort(list(self.__dict__.keys()))
+        for key_ii in keys_sorted:
+            if key_ii not in columns_to_omit:
+                msg += f"\t>>> {key_ii} : {self.__dict__[key_ii]}\n"
+        #
+        msg += "\n" + "-" * 50 + "\n"
+        logger.info(msg)
+
+        return
+
+    def make_copy(self, fill_digits: Optional[str] = 4):
+        # sourcery skip: use-fstring-for-formatting
+        """
+        Function to create a copy of the ``sample_problems.py``.
+
+        Returns
+        ----------
+        filepath : str
+            Path to the copy of the sample Python script.
+        """
+        # Specifying file to copy
+        sample_filepath = (
+            Path(__file__).parent.joinpath("sample_problem.py").resolve()
+        )
+        # Checking that file exists
+        if not sample_filepath.exists():
+            self._check_file_exists(sample_filepath)
+        #
+        # --- Defining output filename
+        output_dirpath = (
+            Path(__file__)
+            .resolve()
+            .parents[1]
+            .joinpath(
+                "src",
+                f'year_{getattr(self, "submission_year")}',
+                f'company_{getattr(self, "company_name")}'.replace("-", "_")
+                .replace(" ", "_")
+                .lower(),
+                getattr(self, "problem_type")
+                .replace("-", "_")
+                .replace(" ", "_")
+                .lower(),
+            )
+        ).resolve()
+        output_dirpath.mkdir(parents=True, exist_ok=True)
+        if files_in_output_dir := np.sort(
+            list(output_dirpath.rglob("*.py"))
+        ).tolist():
+            # Getting the next index
+            last_idx = int(files_in_output_dir[-1].name.split("_")[0])
+            # Determining the new index
+            file_suffix = str(last_idx + 1).zfill(fill_digits)
+        else:
+            file_suffix = "0".zfill(fill_digits)
+        #
+        # --- New filename
+        output_filename = "{}_{}.py".format(
+            file_suffix,
+            getattr(self, "title").replace("-", "_").replace(" ", "_"),
+        )
+        output_filepath = output_dirpath.joinpath(output_filename)
+        #
+        # --- Copying file
+        shutil.copyfile(src=str(sample_filepath), dst=output_filepath)
+        # Check that file exists
+        if not output_filepath.exists():
+            self._check_file_exists(output_filepath)
+        #
+        logger.info(f"New file added: `{output_filepath}`")
+
+    def _check_file_exists(self, arg0):
+        msg = f">>> File `{str(arg0)}` does not exist!"
+        logger.error(msg)
+        raise FileNotFoundError(msg)
+
+
+# ------------------------------ MAIN FUNCTION --------------------------------
+
+
+def main(params_dict: Dict):
+    """
+    Main function of the script.
+    """
+    # Initializing class object
+    create_file_obj = CreateCopyFile(**params_dict)
+    create_file_obj._show_params()
+    create_file_obj.make_copy()
+
+    return
+
+
+if __name__ == "__main__":
+    # Initializing dictionary
+    params_dict = vars(get_parser())
+    # Sending to main function
+    main(params_dict=params_dict)
